46d1d93eb6316226c5ca8a5f95e19a5b4013d750
Jonas Julian Jensen <jojuljen@gmail.com>
2020-04-12 15:53:53 +0700

    Another example error

--- seg7_encoder.vhd
+++ seg7_encoder.vhd
@@ -22,7 +22,7 @@ begin
 
       when 0 => segments <= "0111111";
       when 1 => segments <= "0111111";
-      when 2 => segments <= "1011011";
+      when 2 => segments <= "0111111";
       when 3 => segments <= "1001111";
       when 4 => segments <= "1100110";
       when 5 => segments <= "1101101";
